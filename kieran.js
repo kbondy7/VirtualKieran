77276ad9853a13c561b18b9b3b7c0e3c2a82afa8
Kieran <k.bondman7@gmail.com>
2020-06-13 13:42:14 -0500

    update 1

--- kieran.js
+++ kieran.js
@@ -1,4 +1,6 @@
-var thoughts = ["I love you very very much little potato","You are extremely special human being and there is nobody like you","QTBB MWAH MWAH MWAH", "You have the most beautiful eyes", "Im proud of you!!!!", "You a bad bitch", "You are funny (like for real)", "You have a beautiful smile ;)", "stoopid lady i luv u :P", "Have a great day I'm always here for you sweetie"]
+var thoughts = ["I love you very very much little potato","You are extremely special human being and there is nobody like you","QTBB MWAH MWAH MWAH", "You have the most beautiful eyes", "Im proud of you!!!!", "You a bad bitch", "You are funny (like for real)", "You have a beautiful smile ;)", "stoopid lady i luv u :P", "Have a great day I'm always here for you sweetie",
+"hello princess have a great day!", "I love you bunches", "I miss u more than anything else", "You are so strong and cool I mean look at you", "Take a xan and gimme a hug","u cute as heck",
+"I love your laugh it is so amazing", "<3", "we are yoga masters and our body is our temple", "toot toot", "call real kieran to go on a walk he will do whatever you say", "you are a goddess"]
 
 
 
